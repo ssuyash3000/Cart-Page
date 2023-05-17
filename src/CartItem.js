adf7cfa63b670f8799ca5f17b95fcbec6432d28d
ssuyash3000 <ssuyash2001@gmail.com>
2023-05-18 01:58:52 +0530

    added Increase Quantity function

--- src/CartItem.js
+++ src/CartItem.js
@@ -3,22 +3,49 @@ import plus from './assets/plus.png';
 import minus from './assets/minus.png';
 import deleteIcon from './assets/delete.png';
 class CartItem extends React.Component {
+    constructor (){
+        super();
+        this.state = {
+            price: 999,
+            title: 'Mobile Phone',
+            qty: 1,
+            img: ''
+        }
+      //  this.increaseQuantity = this.increaseQuantity.bind(this);
+    }
+    increaseQuantity = () => {
+        console.log('this qty',this.state.qty);
+    }
     render(){
+        //object De-Structuring
+        const {price, title, qty} = this.state;
         return (
             <div className='cart-item'>
                 <div className='left-block'>
-                    <img style={styles.image}/>
+                    <img alt='product-image' style={styles.image}/>
                 </div>
                 <div className='right-block'>
-                    <div style={{fontSize:25}} >Phone</div>
-                    <div style={{color:'#777'}} >Rs 999</div>
-                    <div style={{color:'#777'}} >Qty: 1</div>
+                    <div style={{fontSize:25}} >{title}</div>
+                    <div style={{color:'#777'}} >Rs {price}</div>
+                    <div style={{color:'#777'}} >Qty: {qty}</div>
                 </div>
                 <div className='cart-item-actions'>
                     {/* Buttons to be added */}
-                    <img alt='increase' className='action-icons' src={plus}/>
-                    <img alt='decrease' className='action-icons'src={minus}/>
-                    <img alt='deleteIcon' className='action-icons' src={deleteIcon}/>
+                    <img 
+                    alt='increase' className='action-icons' 
+                    src={plus}
+                    onClick={this.increaseQuantity}
+                    />
+                    <img 
+                    alt='decrease' className='action-icons'
+                    src={minus}
+                    onClick={this.increaseQuantity}
+                    />
+                    <img
+                    alt='deleteIcon' className='action-icons' 
+                    src={deleteIcon}
+                    onClick={this.increaseQuantity}
+                    />
 
                 </div>
             </div>
