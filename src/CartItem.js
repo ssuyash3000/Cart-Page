7f9f52fa7e54e4e89d53957e94cddc2d86e82cbe
ssuyash3000 <ssuyash2001@gmail.com>
2023-05-19 20:28:21 +0530

    changed cart-item component into functional component, as it donot have any state

--- src/CartItem.js
+++ src/CartItem.js
@@ -2,46 +2,46 @@ import React from 'react';
 import plus from './assets/plus.png';
 import minus from './assets/minus.png';
 import deleteIcon from './assets/delete.png';
-class CartItem extends React.Component {
 
-
-    render(){
-        //object De-Structuring
-        // console.log(this.props)
-        const {product, onIncQuantity,  onDecQuantity, onDeleteProduct} = this.props;
-        const {price, title, qty} = product;
-        return (
-            <div className='cart-item'>
-                <div className='left-block'>
-                    <img alt="productpic" style={styles.image}/>
-                </div>
-                <div className='right-block'>
-                    <div style={{fontSize:25}} >{title}</div>
-                    <div style={{color:'#777'}} >Rs {price}</div>
-                    <div style={{color:'#777'}} >Qty: {qty}</div>
-                </div>
-                <div className='cart-item-actions'>
-                    {/* Buttons to be added */}
-                    <img alt="plusButton"
-                     className='action-icons' 
-                    src={plus}
-                    onClick={()=>{onIncQuantity(product)}}
-                    />
-                    <img alt="minusButton"
-                     className='action-icons'
-                    src={minus}
-                    onClick={()=>{onDecQuantity(product)}}
-                    />
-                    <img alt="deleteButton"
-                     className='action-icons' 
-                    src={deleteIcon}
-                    onClick={()=>{onDeleteProduct(product.id)}}
-                    />
-
-                </div>
+// class CartItem extends React.Component {
+const CartItem = (props) => {
+    // render(){
+    // console.log(this.props)
+    //object De-Structuring
+    const {product, onIncQuantity,  onDecQuantity, onDeleteProduct} = props;
+    const {price, title, qty} = product;
+    return (
+        <div className='cart-item'>
+            <div className='left-block'>
+                <img alt="productpic" style={styles.image}/>
             </div>
-        )
-    }
+            <div className='right-block'>
+                <div style={{fontSize:25}} >{title}</div>
+                <div style={{color:'#777'}} >Rs {price}</div>
+                <div style={{color:'#777'}} >Qty: {qty}</div>
+            </div>
+            <div className='cart-item-actions'>
+                {/* Buttons to be added */}
+                <img alt="plusButton"
+                className='action-icons' 
+                src={plus}
+                onClick={()=>{onIncQuantity(product)}}
+                />
+                <img alt="minusButton"
+                className='action-icons'
+                src={minus}
+                onClick={()=>{onDecQuantity(product)}}
+                />
+                <img alt="deleteButton"
+                className='action-icons' 
+                src={deleteIcon}
+                onClick={()=>{onDeleteProduct(product.id)}}
+                />
+
+            </div>
+        </div>
+    );
+    //}
 }
 const styles = {
     image: {
